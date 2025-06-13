d98f9536993fb1ce8c2cc0c20e62cf8757d8ef1b
JayrGilGaliciaJimenez <20223tn052@utez.edu.mx>
2025-06-12 18:42:25 -0600

    CJ-2 se agrego el menu

--- src/main/java/mx/edu/utez/Main.java
+++ src/main/java/mx/edu/utez/Main.java
@@ -1,7 +1,27 @@
 package mx.edu.utez;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        int opc = 0;
+        while (opc != 5) {
+            System.out.println("""
+                1.  Determinar si un numero es par
+                2.- Determinar si es positivo, negativo o cero
+                3.- Verificar si es multliplo de 5
+                4.- Verificar si es divisible entre 3 y 4 al mismto tiempo
+                5.- Salir
+                """);
+            opc = sc.nextInt();
+            double numero = 0.0;
+
+
+        }
+
+
+
+
     }
 }
